21ef8fd49c33188859668acc6fca80975d58b4fe
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-09-03 13:08:32 +0900

    Update hello.java

--- src/main/java/com/halo/eventer/infra/hello.java
+++ src/main/java/com/halo/eventer/infra/hello.java
@@ -1,4 +1,13 @@
 package com.halo.eventer.infra;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class hello {
+
+    @GetMapping("/health")
+    public void healthCheck(){
+        
+    }
 }
