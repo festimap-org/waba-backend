f0dce4cb5cf4463cb801c26abf6cee6b74416497
BOK-MINJEONG <alswjd2723@gmail.com>
2024-10-11 11:22:27 +0900

    feat: 스플래시 조회 스웨거 추가

---
+++ src/main/java/com/halo/eventer/domain/splash/swagger/SplashGetApi.java
@@ -0,0 +1,22 @@
+package com.halo.eventer.domain.splash.swagger;
+
+import com.halo.eventer.domain.splash.dto.SplashGetDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "스플래시 레이어 전제 조회")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+                content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = SplashGetDto.class)))
+})
+public @interface SplashGetApi {
+}
