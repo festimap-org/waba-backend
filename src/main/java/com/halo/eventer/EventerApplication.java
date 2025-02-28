5af8b0faa082eb57a84fdb0b15bb5dfd3dd4158c
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-01 02:43:12 +0900

    [fix]: 애플리케이션 시간 한국 시간으로 설정

--- src/main/java/com/halo/eventer/EventerApplication.java
+++ src/main/java/com/halo/eventer/EventerApplication.java
@@ -2,11 +2,20 @@ package com.halo.eventer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+
 @SpringBootApplication
 public class EventerApplication {
+
+	@PostConstruct
+	public void init() {
+		// 애플리케이션 시작 시 기본 타임존을 Asia/Seoul로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventerApplication.class, args);
