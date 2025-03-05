faa7e4aac055acc96ce03db6b1bb1f6cbb5fb890
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-05 19:12:14 +0900

    [feat] : Json, Parameter 관련 ExceptionHandler 추가

--- src/main/java/com/halo/eventer/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/halo/eventer/global/error/GlobalExceptionHandler.java
@@ -5,10 +5,15 @@ import com.halo.eventer.global.exception.common.AccessDenyException;
 import com.halo.eventer.global.exception.common.DuplicatedElementException;
 import com.halo.eventer.global.exception.common.NoDataInDatabaseException;
 import java.io.IOException;
+
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -31,12 +36,35 @@ public class GlobalExceptionHandler {
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 
+  // @RequestBody JSON 파싱 에러
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<ErrorResponse> handleJsonParseException(HttpMessageNotReadableException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse.of(ErrorCode.INVALID_JSON_FORMAT));
+  }
+
+  // 필수 @RequestParam 누락
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public ResponseEntity<ErrorResponse> handleMissingParam(MissingServletRequestParameterException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse.of(ErrorCode.MISSING_PARAMETER));
+  }
+
+  // @RequestParam 타입 불일치
+  @ExceptionHandler(TypeMismatchException.class)
+  public ResponseEntity<ErrorResponse> handleTypeMismatchException(TypeMismatchException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse.of(ErrorCode.INVALID_PARAMETER_TYPE));
+  }
+
   /** 기타 에러 예외 처리시 발생 -> 400에러로 메시지 커스텀 하고 싶을 때 사용 */
   @ExceptionHandler(BaseException.class)
   public ResponseEntity<ErrorResponse> handleElementNotFoundException(BaseException e) {
     final ErrorResponse response = ErrorResponse.of(e.getMessage(), e.getErrorCode());
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
+
+
 
   /** IOException */
   @ExceptionHandler(IOException.class)
