cc8955fa127fd32bc4992ade1d8e376fc07059ef
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-05 19:31:46 +0900

    [refactor] : I/OException -> Exception 수정 및 주석 수정

--- src/main/java/com/halo/eventer/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/halo/eventer/global/error/GlobalExceptionHandler.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-  /** 지원하지 않는 HTTP Method로 요청시 발생 */
+  // HTTP Method 불일치 에러
   @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
   public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
       HttpRequestMethodNotSupportedException e) {
@@ -49,18 +49,17 @@ public class GlobalExceptionHandler {
             .body(ErrorResponse.of(ErrorCode.INVALID_PARAMETER_TYPE));
   }
 
-  /** 기타 에러 예외 처리시 발생 -> 400에러로 메시지 커스텀 하고 싶을 때 사용 */
+  // 최상위 domain error
   @ExceptionHandler(BaseException.class)
   public ResponseEntity<ErrorResponse> handleElementNotFoundException(BaseException e) {
     final ErrorResponse response = ErrorResponse.of(e.getMessage(), e.getErrorCode());
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 
-  /** IOException */
-  @ExceptionHandler(IOException.class)
-  public ResponseEntity<String> handleIOException(IOException ex) {
-    // 로깅 또는 추가 처리를 할 수 있습니다.
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body("입출력 예외가 발생했습니다: " + ex.getMessage());
+  // Exception 핸들링
+  @ExceptionHandler(Exception.class)
+  protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+    final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+    return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
