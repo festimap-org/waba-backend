bc9a9e0ea6188d51851f2a40c77fd3e892ef5c79
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-06 22:22:54 +0900

    [test] : FestivalRepository 테스트 코드 작성

---
+++ src/test/java/com/halo/eventer/domain/festival/repository/FestivalRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.halo.eventer.domain.festival.repository;
+
+import com.halo.eventer.domain.festival.Festival;
+import com.halo.eventer.domain.festival.dto.FestivalCreateDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@Disabled()
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SuppressWarnings("NonAsciiCharacters")
+public class FestivalRepositoryTest {
+
+    @Autowired
+    private FestivalRepository festivalRepository;
+
+    private Festival festival;
+
+
+    @BeforeEach
+    void setUp() {
+        FestivalCreateDto dto = new FestivalCreateDto("축제","univ");
+        festival = festivalRepository.save(Festival.from(dto));
+    }
+
+    @Test
+    void 축제생성(){
+        assertThat(festival.getName()).isEqualTo("축제");
+        assertThat(festival.getSubAddress()).isEqualTo("univ");
+    }
+
+
+    @Test
+    void 축제이름으로_축제_찾기(){
+        //when
+        Optional<Festival> result = festivalRepository.findByName("축제");
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+        assertThat(result.get()).isEqualTo(festival);
+    }
+
+    @Test
+    void 축제이름으로_축제_찾기_실패(){
+        //when
+        Optional<Festival> result = festivalRepository.findByName("축");
+
+        //then
+        assertThat(result.isPresent()).isFalse();
+    }
+
+    @Test
+    void subAddress로_축제_찾기(){
+        //when
+        Optional<Festival> result = festivalRepository.findBySubAddress("univ");
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+        assertThat(result.get()).isEqualTo(festival);
+    }
+
+    @Test
+    void subAddress로_축제_찾기_실패(){
+        //when
+        Optional<Festival> result = festivalRepository.findBySubAddress("uni");
+
+        //then
+        assertThat(result.isPresent()).isFalse();
+    }
+}
