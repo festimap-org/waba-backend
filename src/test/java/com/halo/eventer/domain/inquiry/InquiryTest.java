ab6b3e458139bfa689b91ab38f148f6d85852358
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-12 17:15:57 +0900

    [test] : Inquiry 테스트 코드 작성

---
+++ src/test/java/com/halo/eventer/domain/inquiry/InquiryTest.java
@@ -0,0 +1,47 @@
+package com.halo.eventer.domain.inquiry;
+
+import com.halo.eventer.domain.festival.Festival;
+import com.halo.eventer.domain.inquiry.dto.InquiryCreateReqDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class InquiryTest {
+
+    private Festival festival;
+    private Inquiry inquiry;
+    private InquiryCreateReqDto inquiryCreateReqDto;
+
+
+    @BeforeEach
+    void setUp(){
+        Festival festival = new Festival();
+        inquiryCreateReqDto = new InquiryCreateReqDto("제목", false,"아이디","1234","문의내용");
+        inquiry = new Inquiry(festival, inquiryCreateReqDto);
+    }
+
+    @Test
+    void 문의객체_생성(){
+        //when
+        Inquiry target = new Inquiry(festival, inquiryCreateReqDto);
+
+        //then
+        assertThat(target).usingRecursiveComparison()
+                .comparingOnlyFields("title","userId","content","isSecret","password")
+                .isEqualTo(inquiry);
+    }
+
+    @Test
+    void 답변등록(){
+        //given
+        String answer = "답변";
+
+        //when
+        inquiry.registerAnswer(answer);
+
+        //then
+        assertThat(inquiry.getAnswer()).isEqualTo(answer);
+    }
+}
