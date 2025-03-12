890a6612a536a42f2a88381526171d0ad7d735de
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-12 17:13:40 +0900

    [refacotr] : 컬럼명 명시 및 필드명, 메서드명 수정

--- src/main/java/com/halo/eventer/domain/inquiry/Inquiry.java
+++ src/main/java/com/halo/eventer/domain/inquiry/Inquiry.java
@@ -4,6 +4,8 @@ import com.halo.eventer.domain.festival.Festival;
 import com.halo.eventer.domain.inquiry.dto.InquiryCreateReqDto;
 import java.time.LocalDateTime;
 import javax.persistence.*;
+
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -11,35 +13,44 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public class Inquiry {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id",updatable = false)
     private Long id;
 
+    @Column(name="title",nullable = false)
     private String title;
 
-    @Column(nullable = false)
-    private Boolean isSecret;
+    @Column(name = "content", length = 500)
+    private String content;
+
+    @Column(name="answer",length = 500)
+    private String answer;
+
+    @Column(name = "is_secret",nullable = false)
+    private boolean isSecret;
+
+    @Column(name = "is_answered",nullable = false)
+    private boolean isAnswered;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "festival_id")
     private Festival festival;
 
+    @Column(name = "user_id",nullable = false)
     private String userId;
+
+    @Column(name = "password",nullable = false)
     private String password;
 
-    @Column(length = 500)
-    private String content;
-    private boolean isAnswered;
-
-    @Column(length = 500)
-    private String answer;
-
     @CreatedDate
-    private LocalDateTime createdDate;
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
     public Inquiry(Festival festival,InquiryCreateReqDto inquiryCreateReqDto) {
         this.festival = festival;
         this.title = inquiryCreateReqDto.getTitle();
@@ -50,7 +61,7 @@ public class Inquiry {
         this.isAnswered = false;
     }
 
-    public void setAnswer(String answer) {
+    public void registerAnswer(String answer) {
         this.answer = answer;
         this.isAnswered = true;
     }
