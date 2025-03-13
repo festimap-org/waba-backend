ebde1da3b262d97f71376ddfbe6bb62e8b8f2486
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-14 02:26:34 +0900

    [refacotr] : 의미 없는 엔티티 제거

--- src/main/java/com/halo/eventer/domain/concert/exception/ConcertNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.halo.eventer.domain.concert.exception;
-
-import com.halo.eventer.global.error.exception.EntityNotFoundException;
-
-public class ConcertNotFoundException extends EntityNotFoundException {
-    public ConcertNotFoundException(Long id) {
-        super(String.format("Concert with %d is not found", id));
-    }
-}
