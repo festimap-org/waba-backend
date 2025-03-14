55daf0ef98491d63919271f7aac411fc3d48503a
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-14 15:16:23 +0900

    [fix] : 순서 기본값 변경  10 -> 11

--- src/main/java/com/halo/eventer/global/constants/DisplayOrderConstants.java
+++ src/main/java/com/halo/eventer/global/constants/DisplayOrderConstants.java
@@ -1,5 +1,5 @@
 package com.halo.eventer.global.constants;
 
 public class DisplayOrderConstants {
-    public static final int DISPLAY_ORDER_DEFAULT = 10;
+    public static final int DISPLAY_ORDER_DEFAULT = 11;
 }
