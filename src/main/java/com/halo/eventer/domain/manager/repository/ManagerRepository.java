579e99bc3f28141a4b9d0c243f69b8c2dcc027f4
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-15 16:01:58 +0900

    [refacotr] : @Param 명시

--- src/main/java/com/halo/eventer/domain/manager/repository/ManagerRepository.java
+++ src/main/java/com/halo/eventer/domain/manager/repository/ManagerRepository.java
@@ -4,8 +4,9 @@ import com.halo.eventer.domain.manager.Manager;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
     @Query("select m from Manager m where m.festival.id = :id ")
-    public List<Manager> searchManagerByFestivalId(Long id);
+    List<Manager> searchManagerByFestivalId(@Param("id") Long id);
 }
