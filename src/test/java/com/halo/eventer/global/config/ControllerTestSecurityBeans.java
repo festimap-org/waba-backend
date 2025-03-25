3b9f3bc6c48017f22006cc1f29f8209b221f8986
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-26 01:16:50 +0900

    [refactor] : controller 전용 TestConfiguration 빈 추가

--- src/test/java/com/halo/eventer/global/config/ControllerTestSecurityBeans.java
+++ src/test/java/com/halo/eventer/global/config/ControllerTestSecurityBeans.java
@@ -3,6 +3,7 @@ package com.halo.eventer.global.config;
 import com.halo.eventer.global.config.security.AuthorizationConfig;
 import com.halo.eventer.global.config.security.CorsConfig;
 import com.halo.eventer.global.config.security.JwtAuthenticationFilterConfig;
+import com.halo.eventer.global.config.security.SecurityExceptionFilterConfig;
 import com.halo.eventer.global.security.exception.CustomAccessDeniedHandler;
 import com.halo.eventer.global.security.exception.CustomAuthenticationEntryPoint;
 import com.halo.eventer.global.security.provider.JwtProvider;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 
 @TestConfiguration
@@ -40,5 +42,10 @@ public class ControllerTestSecurityBeans {
     @Bean
     public CustomAuthenticationEntryPoint customAuthenticationEntryPoint(){
         return new CustomAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public SecurityExceptionFilterConfig securityExceptionFilterConfig(HandlerExceptionResolver handlerExceptionResolver) {
+        return new SecurityExceptionFilterConfig(handlerExceptionResolver);
     }
 }
