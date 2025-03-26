c15c21afbdcdb1334b0ffed247537c0a4dec883e
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-27 00:22:35 +0900

    [refactor] : Service 변경사항 반영

--- src/main/java/com/halo/eventer/domain/map/controller/MapCategoryController.java
+++ src/main/java/com/halo/eventer/domain/map/controller/MapCategoryController.java
@@ -1,11 +1,9 @@
 package com.halo.eventer.domain.map.controller;
 
-import com.halo.eventer.domain.map.dto.map.MapListDto;
-import com.halo.eventer.domain.map.dto.mapcategory.CategoryEditListDto;
 import com.halo.eventer.domain.map.dto.mapcategory.MapCategoryImageDto;
 import com.halo.eventer.domain.map.dto.mapcategory.MapCategoryResDto;
 import com.halo.eventer.domain.map.service.MapCategoryService;
-import com.halo.eventer.domain.map.swagger.mapcategory.MapGetApi;
+import com.halo.eventer.global.common.dto.OrderUpdateRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,45 +18,36 @@ public class MapCategoryController {
   private final MapCategoryService mapCategoryService;
 
   @PostMapping("/{festivalId}")
-  public List<MapCategoryResDto> createMapCategory(
-      @PathVariable("festivalId") Long festivalId,
-      @RequestParam("categoryName") String categoryName) {
-    return mapCategoryService.createMapCategory(festivalId, categoryName);
-  }
-
-  @PatchMapping("/{categoryId}")
-  public String addImage(
-      @PathVariable("categoryId") Long categoryId,
-      @RequestBody MapCategoryImageDto mapCategoryImageDto) {
-    return mapCategoryService.addIcon(categoryId, mapCategoryImageDto);
-  }
-
-  @GetMapping
-  public List<MapCategoryResDto> getAllCategories(@RequestParam("festivalId") Long festivalId) {
-    return mapCategoryService.getMapCategoryList(festivalId);
+  public List<MapCategoryResDto> create(@PathVariable("festivalId") Long festivalId,
+                                        @RequestParam("categoryName") String categoryName) {
+    return mapCategoryService.create(festivalId, categoryName);
   }
 
   @GetMapping("/image")
-  public MapCategoryImageDto getMapCategoryImages(
-      @RequestParam("mapCategoryId") Long mapCategoryId) {
-    return mapCategoryService.getMapCategoryImages(mapCategoryId);
+  public MapCategoryImageDto getMapCategoryImages(@RequestParam("mapCategoryId") Long mapCategoryId) {
+    return mapCategoryService.getIconAndPin(mapCategoryId);
   }
 
-  @DeleteMapping("/{categoryId}")
-  public List<MapCategoryResDto> deleteMapCategory(
-      @PathVariable("categoryId") Long categoryId, @RequestParam("festivalId") Long festivalId) {
-    return mapCategoryService.deleteMapCategory(categoryId, festivalId);
+  @PatchMapping("/image/{mapCategoryId}")
+  public void updateIconAndPin(@PathVariable("mapCategoryId") Long mapCategoryId,
+                               @RequestBody MapCategoryImageDto mapCategoryImageDto) {
+    mapCategoryService.updateIconAndPin(mapCategoryId, mapCategoryImageDto);
   }
 
-  @MapGetApi
-  @GetMapping("/{mapCategoryId}")
-  public List<MapListDto> getMapList(@PathVariable("mapCategoryId") Long mapCategoryId) {
-    return mapCategoryService.getLandMarks(mapCategoryId);
+  @GetMapping
+  public List<MapCategoryResDto> getMapCategories(@RequestParam("festivalId") Long festivalId) {
+    return mapCategoryService.getMapCategories(festivalId);
   }
 
-  /** 배너 순서 등록 */
-  @PatchMapping("/categoryRank")
-  public String editCategoryRank(@RequestBody CategoryEditListDto categoryEditListDto) {
-    return mapCategoryService.editCategoryRank(categoryEditListDto);
+  @DeleteMapping("/{mapCategoryId}")
+  public List<MapCategoryResDto> delete(@PathVariable("mapCategoryId") Long mapCategoryId,
+                                        @RequestParam("festivalId") Long festivalId) {
+    return mapCategoryService.delete(mapCategoryId, festivalId);
+  }
+
+  @PatchMapping("/displayOrder")
+  public List<MapCategoryResDto> editCategoryRank(@RequestParam("festivalId")Long festivalId,
+                                 @RequestBody List<OrderUpdateRequest> orderUpdateRequests) {
+    return mapCategoryService.updateDisplayOrder(festivalId, orderUpdateRequests);
   }
 }
