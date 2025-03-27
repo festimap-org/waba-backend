de9918623732e3389bb348318f0d5b9e188f1722
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-03-27 21:21:49 +0900

    [refactor] : 안쓰는 메서드 제거

--- src/main/java/com/halo/eventer/domain/duration/repository/DurationMapRepository.java
+++ src/main/java/com/halo/eventer/domain/duration/repository/DurationMapRepository.java
@@ -6,7 +6,5 @@ import com.halo.eventer.domain.duration.DurationMap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DurationMapRepository extends JpaRepository<DurationMap, Long> {
-  void deleteByIdIn(List<Long> ids);
-
   List<DurationMap> findAllByDuration_IdIn(List<Long> ids);
 }
