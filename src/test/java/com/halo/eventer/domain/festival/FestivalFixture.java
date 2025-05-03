e31e43d3e4a676b6b78855f6083d3a2171ab95d6
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-05-03 22:04:05 +0900

    festival 관련 객체 테스트 전용 생성 클래스 추가

---
+++ src/test/java/com/halo/eventer/domain/festival/FestivalFixture.java
@@ -0,0 +1,20 @@
+package com.halo.eventer.domain.festival;
+
+import com.halo.eventer.domain.festival.dto.FestivalCreateDto;
+
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class FestivalFixture {
+
+    public static Festival 축제_엔티티() {
+        return Festival.from(축제_생성용_DTO());
+    }
+
+    public static FestivalCreateDto 축제_생성용_DTO() {
+        FestivalCreateDto festivalCreateDto = new FestivalCreateDto();
+        setField(festivalCreateDto, "name", "축제");
+        setField(festivalCreateDto,"subAddress","주소");
+        return festivalCreateDto;
+    }
+}
