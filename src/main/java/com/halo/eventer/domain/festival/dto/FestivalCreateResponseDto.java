2da5c4e90edb847b112ce68eee2199ecdecfd180
seungin <tmddls91@inha.edu>
2026-03-18 12:27:07 +0900

    [feat] 축제 생성 관련 응답 dto 추가

---
+++ src/main/java/com/halo/eventer/domain/festival/dto/FestivalCreateResponseDto.java
@@ -0,0 +1,17 @@
+package com.halo.eventer.domain.festival.dto;
+
+import com.halo.eventer.domain.festival.Festival;
+import lombok.Getter;
+
+@Getter
+public class FestivalCreateResponseDto {
+    private final Long festivalId;
+    private final String name;
+    private final String subDomain;
+
+    public FestivalCreateResponseDto(Festival festival) {
+        this.festivalId = festival.getId();
+        this.name = festival.getName();
+        this.subDomain = festival.getSubAddress();
+    }
+}
